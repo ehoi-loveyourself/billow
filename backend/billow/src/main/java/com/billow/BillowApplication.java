04a37a7168653b90ad0c73e5f4efe6aca19256bb
eunkyung <bbb381@naver.com>
2022-09-06 16:45:13 +0900

    [BE] FEAT: 프로젝트 초기 세팅

---
+++ backend/billow/src/main/java/com/billow/BillowApplication.java
@@ -0,0 +1,13 @@
+package com.billow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillowApplication.class, args);
+	}
+
+}
---
+++ backend/billow/src/test/java/com/billow/BillowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
