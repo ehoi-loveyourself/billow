6b30219d8315408e8f821ea2dd827c14a1db6d24
kwanukwanu <xoxo70@naver.com>
2022-09-13 16:02:47 +0900

    about view 생성

---
+++ frontend/src/views/AboutView.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="about">
+      <div id="app">
+      <b-container>
+        <header-bar />
+        <b-row>
+          <b-col>
+            <router-view />
+          </b-col>
+        </b-row>
+      </b-container>
+    </div>
+    </div>
+  </template>
+  
+  <script>
+  import HeaderBar from "@/components/layout/HeaderNavBar.vue";
+
+  export default{
+    name:"App",
+    components:{
+      HeaderBar,
+    },
+  }
+  </script>
