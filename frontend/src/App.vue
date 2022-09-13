0c4c53ae043936add05f32bd03e8d75e4428494b
최재현 <xoxo70@naver.com>
2022-09-13 13:25:51 +0900

    footer 수정 및 로고, 로그인 전 배경 asset에 등록

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,55 +1,53 @@
 <template>
   <div id="app">
     <b-container>
-        <router-view />
+      <router-view />
     </b-container>
     <footer-bar />
   </div>
 </template>
 
 <script>
+import FooterBar from '@/components/layout/FooterNavBar.vue';
 
-import FooterBar from "@/components/layout/FooterNavBar.vue";
-
-export default{
-  name:"App",
-  components:{
+export default {
+  name: 'App',
+  components: {
     FooterBar,
   },
-}
+};
 </script>
-
 
 <style>
 html,
 body {
-	height: 100%;
-  margin:0;
+  height: 100%;
+  margin: 0;
 }
 
 a {
-	text-decoration: none;
+  text-decoration: none;
 }
 
 a:hover {
-	text-decoration: none;
-	font-weight: bold;
-	color: chartreuse;
+  text-decoration: none;
+  font-weight: bold;
+  color: chartreuse;
 }
 
 a.router-link-exact-active {
-	color: #064635;
+  color: #064635;
   font-weight: bold;
 }
 
 #app {
-	font-family: basic-scans, sans-serif;
-	font-size: 1em;
-	line-height: 1.6;
-	margin: 0;
-	min-height: 100%;
-	position: relative;
-	padding-bottom: 133px;
+  font-family: basic-scans, sans-serif;
+  font-size: 1em;
+  line-height: 1.6;
+  margin: 0;
+  min-height: 100%;
+  position: relative;
+  padding-bottom: 133px;
   background-color: #141414;
 }
 </style>
