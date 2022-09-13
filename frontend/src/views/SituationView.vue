764e1a948a2167ff77bfce16b70a89124fa1ab41
kwanukwanu <xoxo70@naver.com>
2022-09-13 15:39:13 +0900

    nav bar 설정 수정

--- frontend/src/views/SituationView.vue
+++ frontend/src/views/SituationView.vue
@@ -1,7 +1,16 @@
 <template>
-  <div>상황별 추천 화면</div>
+    <b-container>
+  <router-view>
+    <div>상황별 추천 화면</div>
+  </router-view>
+</b-container>
+
 </template>
 
-<script></script>
+<script>
+  export default {
+    name:"SituationView",
+};
+</script>
 
 <style></style>
