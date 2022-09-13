2fde7d8912d5b67c0338cf9c381c264d0f9679c2
eunkyung <bbb381@naver.com>
2022-09-13 16:39:34 +0900

    [BE] 목업용 API 작성

---
+++ backend/billow/src/main/java/com/billow/exception/BadRequestException.java
@@ -0,0 +1,8 @@
+package com.billow.exception;
+
+public class BadRequestException extends CommonException {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
---
+++ backend/billow/src/main/java/com/billow/exception/CommonException.java
@@ -0,0 +1,8 @@
+package com.billow.exception;
+
+public class CommonException extends RuntimeException {
+
+    public CommonException(String message) {
+        super(message);
+    }
+}
