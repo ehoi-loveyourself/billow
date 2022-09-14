fc97bfc5c3dfd4ada3a9337bd8d7d26a93679233
안태환 <atj14@naver.com>
2022-09-14 10:40:35 +0900

    [FE] FEAT: 상황별 추천 초기 화면 구성

--- frontend/src/views/SituationView.vue
+++ frontend/src/views/SituationView.vue
@@ -1,12 +1,18 @@
 <template>
   <router-view>
+    <SituationIntroduction></SituationIntroduction>
     <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
   </router-view>
 </template>
 
 <script>
+  import SituationIntroduction from "@/components/situation/SituationIntroduction.vue";
+
   export default {
     name:"SituationView",
+    components:{
+    SituationIntroduction,
+},
 };
 </script>
 
