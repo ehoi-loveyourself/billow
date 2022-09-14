aa95f1cf000a9f3c1b12086a844d829e14b509aa
belle <ehoi.loveyourself@gmail.com>
2022-09-14 21:09:09 +0900

    [BE] FEAT : 평점 조회시 Response할 DTO 생성

---
+++ backend/billow/src/main/java/com/billow/domain/dto/addtion/RatingResponse.java
@@ -0,0 +1,15 @@
+package com.billow.domain.dto.addtion;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RatingResponse {
+
+    private String title;
+    private Float score;
+}
