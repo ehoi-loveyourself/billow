32042bb963c60c9fe76bffe17db570cf5d63e225
안태환 <atj14@naver.com>
2022-09-14 17:45:21 +0900

    [FE] FEAT: 메인 화면 포스터 카드

--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -1,18 +1,22 @@
 <template>
     <router-view>
         <br>
-        <div style="text-align:center">
+      <div style="text-align:center">
         <br/><br/>
         <img src="@/assets/image_1.png"/>
         <br/><br/><br/><br/><br/>
       </div>
+      <MainOnAirProgram></MainOnAirProgram>
     </router-view>
   </template>
   
   <script>
+import MainOnAirProgram from "../components/main/MainOnAirProgram.vue";
+
     export default {
-      name:"MainView",
-  };
+    name: "MainView",
+    components: { MainOnAirProgram }
+};
   </script>
   
   <style></style>
