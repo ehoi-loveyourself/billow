39041a553812d4d5945e94a3f60ac0f748816f8a
kwanukwanu <xoxo70@naver.com>
2022-09-14 18:07:22 +0900

    [FE] FEAT: 하단 추천 세로 포스터로 변경 및 css 수정

--- frontend/src/components/main/MainOnAirProgram.vue
+++ frontend/src/components/main/MainOnAirProgram.vue
@@ -3,7 +3,7 @@
 <h2 style="color:white">지금 방송 중인 프로그램(ON AIR)</h2>
 
 <div class="card" style="color:white">
-  <img src="@/assets/image_1.png" alt="Image" style="width:70%">
+  <img src="@/assets/image_2.png" alt="Image" style="width:40%">
   <div class="container">
     <h4><b>호버 했을 때 제목 등등</b></h4> 
     <p>카드 형식 사용</p>
@@ -13,7 +13,7 @@
 <h2 style="color:white">요즘 핫한 프로그램</h2>
 
 <div class="card" style="color:white">
-  <img src="@/assets/image_1.png" alt="Image" style="width:70%">
+  <img src="@/assets/image_2.png" alt="Image" style="width:40%">
   <div class="container">
     <h4><b>카드 형식</b></h4> 
     <p>카드 형식 사용</p> 
@@ -25,10 +25,10 @@
 
 <style>
   .card {
-    background-color: blue;
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    background-color: #a1a1a1;
+    box-shadow: 0 4px 6px 0 rgba(0, 0, 0, 0.2);
     transition: 0.3s;
-    width: 40%;
+    width: 30%;
   }
   
   .card:hover {
