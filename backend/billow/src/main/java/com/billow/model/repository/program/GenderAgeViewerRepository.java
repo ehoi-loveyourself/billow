b1dcafcb82d02f60b3a6c13435b2bec9816e374a
eunkyung <bbb381@naver.com>
2022-09-21 15:32:59 +0900

    [BE] FEAT : 성연령별 프로그램 추천 API

--- backend/billow/src/main/java/com/billow/model/repository/program/GenderAgeViewerRepository.java
+++ backend/billow/src/main/java/com/billow/model/repository/program/GenderAgeViewerRepository.java
@@ -4,10 +4,11 @@ import com.billow.domain.entity.program.GenderAgeViewer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface GenderAgeViewerRepository extends JpaRepository<GenderAgeViewer, Long> {
+public interface GenderAgeViewerRepository extends JpaRepository<GenderAgeViewer, Long>, GenderAgeViewerCustomRepository {
 
     GenderAgeViewer findByProgramTitle(String programTitle);
 }
--- backend/billow/src/main/java/com/billow/model/repository/program/ProgramCustomRepository.java
+++ backend/billow/src/main/java/com/billow/model/repository/program/ProgramCustomRepository.java
@@ -1,5 +1,6 @@
 package com.billow.model.repository.program;
 
+import com.billow.domain.dto.program.ProgramResponse;
 import com.billow.domain.entity.program.Program;
 
 import java.util.List;
@@ -7,4 +8,6 @@ import java.util.List;
 public interface ProgramCustomRepository {
 
     List<Program> findPopularProgram();
+
+    List<Program> findGenderAgeRecommend(Long userId, Integer age, String gender, Long id, Long id1, Long id2, Long id3, Long id4);
 }
