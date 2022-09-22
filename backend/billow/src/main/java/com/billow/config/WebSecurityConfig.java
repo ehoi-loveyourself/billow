7511b37924fa52a26a89dfa2742dd40a48e990f9
박태이 <ehoi.loveyourself@gmail.com>
2022-09-22 16:41:52 +0900

    [BE] FEAT : SecurityConfig 설정

--- backend/billow/src/main/java/com/billow/config/WebSecurityConfig.java
+++ backend/billow/src/main/java/com/billow/config/WebSecurityConfig.java
@@ -1,22 +1,27 @@
 package com.billow.config;
 
+import com.billow.util.JwtAuthenticationFilter;
+import com.billow.util.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
 
     //    @Override
 //    public void configure(WebSecurity web) throws Exception {
@@ -28,12 +33,18 @@ public class WebSecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.cors().and().csrf().disable();
         http
                 .httpBasic().disable()
                 .csrf().disable()
+                .authorizeRequests() // 요청에 대한 사용권한 체크
+                .antMatchers("/api/users/validation").permitAll()
+                .antMatchers("/api/users/kakaoLogin").permitAll()
+                .antMatchers("/api/users/signup").permitAll()
+                .antMatchers("/api/users/refresh").permitAll()
+                .anyRequest().authenticated();
         ;
 //                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
 //                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
@@ -44,18 +55,10 @@ public class WebSecurityConfig {
 //                .antMatchers("/api/test/**").permitAll()
 //                .anyRequest().authenticated();
 //
-//        http.addFilterBefore(authenticationJwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
-
-    //@Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .httpBasic().disable()
-//                .csrf().disable()
-//        ;
-//    }
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
