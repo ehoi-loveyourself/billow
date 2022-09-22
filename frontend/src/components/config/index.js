582e39235630f735373f91af7905a731b8f30869
kwanukwanu <xoxo70@naver.com>
2022-09-22 12:55:59 +0900

    [FE] FIX: favicon 코드 수정

--- frontend/src/components/config/index.js
+++ frontend/src/components/config/index.js
@@ -1,5 +1,5 @@
 // 관련 데이터 URL을 저장하는 부분
-const API_BASE_URL = "j7b309.p.ssafy.io/api";
+const API_BASE_URL = "j7b309.p.ssafy.io";
 // const API_BASE_URL = "http://localhost:8080/api";
 
 export { API_BASE_URL };
