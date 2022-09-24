8837ed2a80edb16651f24f689848fcec70ad742c
안태환 <atj14@naver.com>
2022-09-24 16:06:53 +0900

    [FE] FEAT : axios

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import axios from "axios";
 import BootstrapVue3 from 'bootstrap-vue-3'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
-
 const app = createApp(App);
+app.config.globalProperties.$axios = axios;
 app.use(router).use(BootstrapVue3).mount('#app');
