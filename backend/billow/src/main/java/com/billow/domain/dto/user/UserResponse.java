6cb822b3bbdf8129925cf62f541fa5e59c046a04
belle <ehoi.loveyourself@gmail.com>
2022-09-24 12:18:07 +0900

    [BE] FEAT : 유저 정보 반환할 dto 생성

---
+++ backend/billow/src/main/java/com/billow/domain/dto/user/UserResponse.java
@@ -0,0 +1,28 @@
+package com.billow.domain.dto.user;
+
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserResponse {
+
+    private String email;
+
+    private String name;
+
+    private String nickName;
+
+    private String gender;
+
+    private Integer age;
+
+    private String region;
+
+    private String tvCarrier;
+
+    private String profileImgUrl;
+
+    private String mobile;
+}
