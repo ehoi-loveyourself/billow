3002cc5966b8eb88b0c97a953c23e4e9407b669c
jihoyangKR <97648339+jihoyangKR@users.noreply.github.com>
2022-09-26 17:54:27 +0900

    [BE] FEAT : WebClient build.grade수정, Controller 수정

--- backend/billow/src/main/java/com/billow/controller/program/DjangoRecommandController.java
+++ backend/billow/src/main/java/com/billow/controller/program/DjangoRecommandController.java
@@ -1,8 +1,8 @@
 package com.billow.controller.program;
 
+import com.billow.domain.dto.program.ProgramResponse;
 import com.billow.domain.entity.program.Program;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,18 +12,16 @@ import reactor.core.publisher.Flux;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/userrecommend")
+@RequestMapping("/user-recommend")
 public class DjangoRecommandController {
 
     public final WebClient webClient;
 
-    @GetMapping("/uesrRecomm/{userId}")
-    public Flux<Program> userRecommend(@PathVariable("userId") Long userId) {
+    @GetMapping("/{userId}")
+    public Flux<ProgramResponse> userRecommend(@PathVariable("userId") Long userId) {
         return webClient.get()
                 .uri("db/" + userId + "/")
                 .retrieve()
-                .bodyToFlux(Program.class);
-
-
-    }
+                .bodyToFlux(ProgramResponse.class);
+        }
 }
