ce840ecf063279491c071cdb385b8a0ab18eab95
박태이 <ehoi.loveyourself@gmail.com>
2022-09-26 15:03:57 +0900

    [BE] FEAT : WebSecurityCustomizer 빈 등록

--- backend/billow/src/main/java/com/billow/config/WebSecurityConfig.java
+++ backend/billow/src/main/java/com/billow/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.billow.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -9,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -28,12 +30,31 @@ public class WebSecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .antMatchers("/api/recommend/popular");
+    }
+
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.cors().and().csrf().disable();
         http
                 .httpBasic().disable()
                 .csrf().disable()
+//                .authorizeRequests()
+//                .antMatchers("/api/users/validation/**").permitAll()
+//                .antMatchers("/api/users/oauth/**").permitAll()
+//                .antMatchers("/api/users/signup").permitAll()
+//                .antMatchers("/api/users/refresh").permitAll()
+//                .antMatchers("/api/recommend/new").permitAll()
+//                .antMatchers("/api/recommend/popular").permitAll()
+//                .antMatchers("/api/recommend/onair").permitAll()
+//                .antMatchers("/api/organization/**").permitAll()
+//                .antMatchers(HttpMethod.GET, "/api/chat/**").permitAll()
+//                .antMatchers("/api/data/**").permitAll()
+//                .anyRequest().authenticated()
         ;
 //                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
 //                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
