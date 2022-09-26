e519e6e303c5ea92014379cdb8a10b44f1476883
박태이 <ehoi.loveyourself@gmail.com>
2022-09-26 16:59:42 +0900

    [BE] FIX : 랜덤 프로그램 출력 수정

--- backend/billow/src/main/java/com/billow/controller/program/ProgramController.java
+++ backend/billow/src/main/java/com/billow/controller/program/ProgramController.java
@@ -53,7 +53,7 @@ public class ProgramController {
     @GetMapping("/random")
     public ResponseEntity<Object> randomProgram(@RequestHeader("Auth-access") String token) {
         log.info("사용자 초기 데이터 수집용 랜덤 프로그램 출력 API 호출");
-        List<ProgramResponse> response = programService.randomProgram();
+        List<ProgramResponse> response = programService.randomProgram(JwtUtil.getUserId(token));
         log.info("랜덤 프로그램 출력 성공");
         return ResponseEntity.ok()
                 .body(response);
