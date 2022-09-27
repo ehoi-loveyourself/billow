4e1802edc9b7957cf2d7cc53b17c14f6465576a8
belle <ehoi.loveyourself@gmail.com>
2022-09-27 21:56:41 +0900

    [BE] FEAT : 401 예외처리 추가

---
+++ backend/billow/src/main/java/com/billow/exception/UnauthorizedException.java
@@ -0,0 +1,8 @@
+package com.billow.exception;
+
+public class UnauthorizedException extends CommonException {
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
