022e8e96b58438cb23b3ba53be1ebb033e799860
jihoyangKR <97648339+jihoyangKR@users.noreply.github.com>
2022-09-28 11:41:57 +0900

    FIX : 프로그램 중복 에러 해결

--- backend/djangoBack/datas/models.py
+++ backend/djangoBack/datas/models.py
@@ -8,6 +8,120 @@
 from django.db import models
 
 
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.IntegerField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.IntegerField()
+    is_active = models.IntegerField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.PositiveSmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
+
 class TbBookmark(models.Model):
     bookmark_id = models.BigAutoField(primary_key=True)
     program = models.ForeignKey('TbProgram', models.DO_NOTHING)
@@ -23,6 +137,8 @@ class TbBroadcastingAlarm(models.Model):
     date_time = models.CharField(max_length=255)
     program = models.ForeignKey('TbProgram', models.DO_NOTHING)
     user = models.ForeignKey('TbUser', models.DO_NOTHING)
+    group_id = models.CharField(max_length=255)
+    program_organization = models.ForeignKey('TbProgramOrganization', models.DO_NOTHING)
 
     class Meta:
         managed = False
@@ -263,10 +379,8 @@ class TbProgram(models.Model):
     poster_img = models.CharField(max_length=255, blank=True, null=True)
     program_num = models.IntegerField(blank=True, null=True)
     rating_cnt = models.BigIntegerField(blank=True, null=True)
-    summary = models.CharField(max_length=1000, blank=True, null=True)
+    summary = models.CharField(max_length=10000, blank=True, null=True)
     title = models.CharField(max_length=255, blank=True, null=True)
-
-    # genres = models.ManyToManyField(TbGenreInfo)
 
     class Meta:
         managed = False
@@ -340,6 +454,7 @@ class TbUser(models.Model):
     profile_img = models.ForeignKey(TbProfileImg, models.DO_NOTHING, blank=True, null=True)
     region = models.ForeignKey(TbRegion, models.DO_NOTHING, blank=True, null=True)
     tv_carrier = models.ForeignKey(TbTvCarrier, models.DO_NOTHING, blank=True, null=True)
+    mobile = models.CharField(max_length=255, blank=True, null=True)
 
     class Meta:
         managed = False
