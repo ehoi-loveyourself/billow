ceb26551463d8b60b9d841ef26ecb50526b5570e
belle <ehoi.loveyourself@gmail.com>
2022-09-28 22:04:03 +0900

    [BE] FEAT : 인터셉터 주석

--- backend/billow/src/main/java/com/billow/config/WebConfig.java
+++ backend/billow/src/main/java/com/billow/config/WebConfig.java
@@ -1,10 +1,10 @@
 package com.billow.config;
 
-import com.billow.jwt.JwtAuthInterceptor;
+//import com.billow.jwt.JwtAuthInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -35,8 +35,8 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowCredentials(true);
     }
 
-    public void addInterceptors(InterceptorRegistry interceptorRegistry) {
-        interceptorRegistry.addInterceptor(new JwtAuthInterceptor())
-                .excludePathPatterns(EXCLUDED_PATHS);
-    }
+//    public void addInterceptors(InterceptorRegistry interceptorRegistry) {
+//        interceptorRegistry.addInterceptor(new JwtAuthInterceptor())
+//                .excludePathPatterns(EXCLUDED_PATHS);
+//    }
 }
--- backend/billow/src/main/java/com/billow/jwt/JwtAuthInterceptor.java
+++ backend/billow/src/main/java/com/billow/jwt/JwtAuthInterceptor.java
@@ -1,53 +1,58 @@
-package com.billow.jwt;
-
-import com.billow.exception.BadRequestException;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.servlet.HandlerInterceptor;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-@Slf4j
-@RequiredArgsConstructor
-public class JwtAuthInterceptor implements HandlerInterceptor {
-
-    private static final String CHAT_REQUEST = "/api/chat/**";
-    private static final String PROGRAM_REQUEST = "/api/program/**";
-    private static final String REVIEW_REQUEST = "/api/review/**";
-    private static final String HEADER = "Auth-access";
-    private static final String NO_TOKEN = "재로그인이 필요합니다.";
-
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        String requestURI = request.getRequestURI();
-        String requestMethod = request.getMethod();
-        log.info("요청 URI: " + requestURI);
-        log.info("요청 Method:" + requestMethod);
-        if (CHAT_REQUEST.equals(requestURI) && HttpMethod.GET.matches(requestMethod)
-                || PROGRAM_REQUEST.equals(requestURI) && HttpMethod.GET.matches(requestMethod)
-                || REVIEW_REQUEST.equals(requestURI) && HttpMethod.GET.matches(requestMethod)
-                || requestURI.equals("/api/users/validation/**")
-                || requestURI.equals("/api/users/oauth")
-                || requestURI.equals("/api/users/signup")
-                || requestURI.equals("/api/users/refresh")
-                || requestURI.equals("/api/profile/**")
-                || requestURI.equals("/api/recommend/new")
-                || requestURI.equals("/api/recommend/popular")
-                || requestURI.equals("/api/recommend/onair")
-                || requestURI.equals("/api/organization/**")) {
-            // TODO 그냥 바로 true 줄 애들
-            return true;
-        }
-
-        String token = request.getHeader(HEADER);
-        if (token == null) {
-            throw new BadRequestException(NO_TOKEN);
-        }
-        if (JwtUtil.validateToken(token)) {
-            return true;
-        }
-        return true;
-    }
-}
+//package com.billow.jwt;
+//
+//import com.billow.exception.BadRequestException;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.web.servlet.HandlerInterceptor;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.util.regex.Pattern;
+//
+//@Slf4j
+//@RequiredArgsConstructor
+//public class JwtAuthInterceptor implements HandlerInterceptor {
+//
+//    private static final String CHAT_REQUEST = "/api/chat/**";
+////    private static final String PROGRAM_REQUEST = "/api/program/**";
+//    private static final Pattern PROGRAM_REQUEST = Pattern.compile("/api/program/[.]");
+//    private static final Pattern REVIEW_REQUEST = Pattern.compile("/api/review/[0-9]");
+//    private static final String HEADER = "Auth-access";
+//    private static final String NO_TOKEN = "재로그인이 필요합니다.";
+//
+//    @Override
+//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//        String requestURI = request.getRequestURI();
+//        String requestMethod = request.getMethod();
+//        log.info("요청 URI: " + requestURI);
+//        log.info("요청 Method:" + requestMethod);
+//        if (CHAT_REQUEST.equals(requestURI) && HttpMethod.GET.matches(requestMethod)
+//                || PROGRAM_REQUEST.matcher(requestURI).matches() && HttpMethod.GET.matches(requestMethod)
+//                || REVIEW_REQUEST.matcher(requestURI).matches() && HttpMethod.GET.matches(requestMethod)
+//                || requestURI.equals("/api/users/validation/**")
+//                || requestURI.equals("/api/users/oauth")
+//                || requestURI.equals("/api/users/signup")
+//                || requestURI.equals("/api/users/refresh")
+//                || requestURI.equals("/api/profile/**")
+//                || requestURI.equals("/api/recommend/new")
+//                || requestURI.equals("/api/recommend/popular")
+//                || requestURI.equals("/api/recommend/onair")
+//                || requestURI.equals("/api/organization/**")
+//                || requestURI.equals("/api/program/random")
+//                || requestURI.matches("/api/program/cast/[0-9]]")
+//        ) {
+//            // TODO 그냥 바로 true 줄 애들
+//            log.info("if문 걸렸어");
+//            return true;
+//        }
+//        log.info("if문 안걸렸어");
+//        String token = request.getHeader(HEADER);
+//        if (token == null) {
+//            throw new BadRequestException(NO_TOKEN);
+//        }
+//        // 만료됐을때
+//        JwtUtil.validateToken(token);
+//        return true;
+//    }
+//}
