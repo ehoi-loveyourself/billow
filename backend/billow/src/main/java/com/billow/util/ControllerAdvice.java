6de374ba51643afcfe695d61259a6766395d25ca
박태이 <ehoi.loveyourself@gmail.com>
2022-09-29 14:48:35 +0900

    [BE] FEAT : 디벨롭에 있는 exception 가져오기

--- backend/billow/src/main/java/com/billow/util/ControllerAdvice.java
+++ backend/billow/src/main/java/com/billow/util/ControllerAdvice.java
@@ -47,11 +47,11 @@ public class ControllerAdvice {
         return new Message(runtimeException.getMessage());
     }
 
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler(Exception.class)
-    public Message Exception(RuntimeException runtimeException) {
-        log.info(runtimeException.getMessage());
-        return new Message(runtimeException.getMessage());
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({RuntimeException.class, Exception.class})
+    public Message commonException(Exception e) {
+        log.info(e.getMessage());
+        return new Message("오류가 발생했습니다.");
     }
 
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
