9911ee277b9bba8ceb01f1ccb05cc29316aff194
박태이 <ehoi.loveyourself@gmail.com>
2022-09-29 16:58:10 +0900

    Apply 1 suggestion(s) to 1 file(s)

--- backend/billow/src/main/java/com/billow/model/repository/addition/ChatRepository.java
+++ backend/billow/src/main/java/com/billow/model/repository/addition/ChatRepository.java
@@ -7,6 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ChatRepository extends JpaRepository<Chat, Long> {
-//    List<Chat> findByProgram_Id(Long programId);
     List<Chat> findByProgram(Program program);
 }
