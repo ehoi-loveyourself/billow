6532071e6e5a6e44a65254d0fb51a09752c2160d
kwanukwanu <xoxo70@naver.com>
2022-09-30 14:40:04 +0900

    [FE] FIX: 맞춤 추천 가로 포스터 캡션 추가

--- frontend/src/components/main/UserRecommend.vue
+++ frontend/src/components/main/UserRecommend.vue
@@ -1,13 +1,22 @@
 <template>
     <div id="mainslider" style="text-align:center; margin-top: 10px; ">
         <splide id="carousel_user_recommend" :options="options">
+          <splide-slide>
+          <figure>
+        <img class="img_2" src="https://image.tmdb.org/t/p/original/9xIIKJkvMirs3aA69FV19Ckx0fa.jpg" alt="main2">
+        <figcaption>블라인드</figcaption>
+      </figure>
+        </splide-slide>
         <splide-slide><a class="enterDetail" href="#">
           <router-link :to="{ name: 'detail' }" class="nav-link">
         <img class="img_1" src="@/assets/image_180.png" alt="main1"></router-link>
       </a>
         </splide-slide>
         <splide-slide>
+          <figure>
         <img class="img_2" src="https://image.tmdb.org/t/p/original/FbUIi5WeDU5j1EtipQ274tts1E.jpg" alt="main2">
+        <figcaption>으라차차 내 인생</figcaption>
+      </figure>
         </splide-slide>
         <splide-slide>
         <img class="img_1" src="@/assets/daedang.png" alt="main3">
@@ -53,7 +62,8 @@
   }
   }
 </script>
-  
+
+
 <style>
 #carousel_user_recommend .splide__arrow--next{
     right:0.5em;
@@ -85,5 +95,46 @@
 }
 .splide__arrow{
   width:fit-content;
+}
+</style>
+
+<style scoped>
+  figcaption {
+  color: #fff;
+  background: rgba(55, 55, 55, 0.6);
+  border-radius: 0.8vw;
+  font-weight: 600;
+  padding: 0.7em;
+  padding-right: 0.2em;
+  padding-left: 0.2em;
+  /* line-height: 100; */
+  position: absolute;
+  left: 5%;
+  bottom: 9%;
+  width: fit-content;
+  font-size: 2.4vw;
+}
+
+figure {
+  margin: 0;
+  line-height: 0;
+  
+  position: relative;
+}
+
+/* Alignment styles */
+body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: system-ui;
+}
+
+body, html {
+  height: 100%;
+}
+
+* {
+  box-sizing: border-box;
 }
 </style>
