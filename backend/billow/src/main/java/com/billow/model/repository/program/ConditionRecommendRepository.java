acc439c84ad75d5fc38e2f00affbdbf324cc104a
박태이 <ehoi.loveyourself@gmail.com>
2022-10-01 18:49:51 +0900

    [BE] FEAT : 해당 기분과 장르에 따라 상위 3개 프로그램을 select 하는 query 작성

--- backend/billow/src/main/java/com/billow/model/repository/program/ConditionRecommendRepository.java
+++ backend/billow/src/main/java/com/billow/model/repository/program/ConditionRecommendRepository.java
@@ -3,5 +3,5 @@ package com.billow.model.repository.program;
 import com.billow.domain.entity.program.ConditionRecommend;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ConditionRecommendRepository extends JpaRepository<ConditionRecommend, Long> {
+public interface ConditionRecommendRepository extends JpaRepository<ConditionRecommend, Long>, ConditionRecommendCustomRepository {
 }
