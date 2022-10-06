ac2848b3577f38724464d24facbaae00c87345df
jihoyangKR <97648339+jihoyangKR@users.noreply.github.com>
2022-10-06 23:26:25 +0900

    [BE] FIX : recomm 오류 수정, posterPath 경로 수정

--- backend/djangoBack/datas/recomm.py
+++ backend/djangoBack/datas/recomm.py
@@ -1,5 +1,6 @@
 from copyreg import pickle
 from contextlib import redirect_stderr
+from operator import le
 from sklearn.decomposition import TruncatedSVD
 from scipy.sparse.linalg import svds
 
@@ -48,6 +49,8 @@ def predict_table():
     df_rating = query_MySQL('SELECT score, program_id, user_id from tb_rating')
     user_list = query_MySQL('SELECT * From tb_user')
 
+    user_list.set_index('user_id', drop=True, append=False, inplace=False)
+
     user_list = user_list.values.tolist()
     users = []
     for user in user_list:
@@ -72,31 +75,79 @@ def predict_table():
 
     svd_user_predicted_ratings = np.dot(np.dot(U, sigma), Vt) + user_ratings_mean.reshape(-1, 1)
 
+    print(svd_user_predicted_ratings)
+
     df_svd_preds = pd.DataFrame(svd_user_predicted_ratings, columns = df_user_program_ratings.columns)
-    # df_svd_preds.head()
+    # df_svd_preds = pd.DataFrame(svd_user_predicted_ratings, columns = user_list)
+
+    df_svd_preds.head()
+
+    print('#################################')
+    print(df_svd_preds)                     
+    print('##################################')
 
     return users, df_program, df_rating, df_svd_preds
 
-def recommend_programs(df_svd_preds, user_id, ori_programs_df, ori_ratings_df, num_recommendations=10):
+# def recommend_programs(df_svd_preds, user_id, ori_programs_df, ori_ratings_df, num_recommendations=10):
+#     # user_id = user_id
+#     #현재는 index로 적용이 되어있으므로 user_id - 1을 해야함.
+#     df_svd_preds.set_index(user_id, inplace=True)
+
+#     # 최종적으로 만든 pred_df에서 사용자 index에 따라 프로그램 데이터 정렬 -> 프로그램 평점이 높은 순으로 정렬 됌
+#     sorted_user_predictions = df_svd_preds.iloc[user_id].sort_values(ascending=False)
+
+#     # sorted_user_predictions = df_svd_preds.loc[df_svd_preds["user_id"]==user_id].sort_values(ascending=False)
+
+#     # 원본 평점 데이터에서 user id에 해당하는 데이터를 뽑아낸다. 
+#     user_data = ori_ratings_df[ori_ratings_df.user_id == user_id]
+
+#     # 위에서 뽑은 user_data와 원본 프로그램 데이터를 합친다. 
+#     user_history = user_data.merge(ori_programs_df, on = 'program_id').sort_values(['score'], ascending=False)
+
+#     # 원본 프로그램 데이터에서 사용자가 본 프로그램 데이터를 제외한 데이터를 추출
+#     recommendations = ori_programs_df[~ori_programs_df['program_id'].isin(user_history['program_id'])]
+#     # 사용자의 프로그램 평점이 높은 순으로 정렬된 데이터와 위 recommendations을 합친다. 
+#     recommendations = recommendations.merge( pd.DataFrame(sorted_user_predictions).reset_index(), on = 'program_id')
+#     # 컬럼 이름 바꾸고 정렬해서 return
+#     # recommendations = recommendations.rename(columns = {user_id: 'Predictions'}).sort_values('Predictions', ascending = False).iloc[:num_recommendations, :]
+
+#     recommendations = recommendations.sort_values(user_id, ascending = False).iloc[:num_recommendations, :]
+
+                        
+
+#     return recommendations
+
+def recommend_programs(df_svd_preds, i, user_id, ori_programs_df, ori_ratings_df, num_recommendations=10):
 
     #현재는 index로 적용이 되어있으므로 user_id - 1을 해야함.
+    user_row_number = user_id - 1 
+    print(i, user_id,"###########")
+
+    # i = i - 1
 
     # 최종적으로 만든 pred_df에서 사용자 index에 따라 프로그램 데이터 정렬 -> 프로그램 평점이 높은 순으로 정렬 됌
-    sorted_user_predictions = df_svd_preds.df["user_id"].sort_values(ascending=False)
-
+    sorted_user_predictions = df_svd_preds.iloc[i].sort_values(ascending=False)
+    # sorted_user_predictions = df_svd_preds.loc[user_row_number].sort_values(ascending=False)
     # 원본 평점 데이터에서 user id에 해당하는 데이터를 뽑아낸다. 
     user_data = ori_ratings_df[ori_ratings_df.user_id == user_id]
 
     # 위에서 뽑은 user_data와 원본 프로그램 데이터를 합친다. 
-    user_history = user_data.merge(ori_programs_df, on = 'program_id').sort_values(['score'], ascending=False)
+    # user_history = user_data.merge(ori_programs_df, on = 'program_id').sort_values(['score'], ascending=False)
 
     # 원본 프로그램 데이터에서 사용자가 본 프로그램 데이터를 제외한 데이터를 추출
-    recommendations = ori_programs_df[~ori_programs_df['program_id'].isin(user_history['program_id'])]
+    # recommendations = ori_programs_df[~ori_programs_df['program_id'].isin(user_history['program_id'])]
+    recommendations = ori_programs_df[~ori_programs_df['program_id'].isin(user_data['program_id'])]
+
     # 사용자의 프로그램 평점이 높은 순으로 정렬된 데이터와 위 recommendations을 합친다. 
-    recommendations = recommendations.merge( pd.DataFrame(sorted_user_predictions).reset_index(), on = 'program_id')
+    recommendations = recommendations.merge(pd.DataFrame(sorted_user_predictions).reset_index(), on = 'program_id')
+
+    print(recommendations)
     # 컬럼 이름 바꾸고 정렬해서 return
-    recommendations = recommendations.rename(columns = {user_id: 'Predictions'}).sort_values('Predictions', ascending = False).iloc[:num_recommendations, :]
-                        
+    recommendations = recommendations.rename(columns = {i: 'Predictions'}).sort_values('Predictions', ascending = False)#.iloc[:num_recommendations, :]
+    print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+    print(recommendations)
+
+    print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
 
     return recommendations
 
@@ -122,25 +173,28 @@ def recommend_programs(df_svd_preds, user_id, ori_programs_df, ori_ratings_df, n
 def mf_algo_individual(request):
     user_id = request
     users, df_program, df_rating, df_svd_preds = predict_table()
-    indi_predict_result = pd.DataFrame()
-    indi_user_result = recommend_programs(df_svd_preds, user_id, df_program, df_rating)
-    print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-    print(user_id)
-    print('_---------------------------------------')
-    print(indi_predict_result)
-    print(indi_user_result)
-    print('_---------------------------------------')
+    num = len(users)
+    print(users, num)
+    predict_result = pd.DataFrame()
+    for i, user in enumerate(users):
+        if user_id == user:
 
-    indi_user_result.insert(1, 'user_id', user_id)
-    print('_---------------------------------------')
-    print(indi_user_result)
-    print('_---------------------------------------')
+            user_result = recommend_programs(df_svd_preds, i, user, df_program, df_rating)
+            user_result.insert(2, 'user_id', user)
+            user_result = user_result[0:10]
+            print(user_result)
+            predict_result = pd.concat([predict_result, user_result])
+            print(predict_result)
 
-    indi_user_result = indi_user_result[0:10]
+    # indi_user_result = recommend_programs(df_svd_preds, user_id, df_program, df_rating)
 
-    indi_predict_result = pd.concat([indi_predict_result, indi_user_result])
+    # indi_user_result.insert(1, 'user_id', user_id)
+
+    # indi_user_result = indi_user_result[0:10]
+
+    # indi_predict_result = pd.concat([indi_predict_result, indi_user_result])
     print('##########################################')
-    print(indi_predict_result)
+    print(predict_result)
     print('##########################################')
 
     # with open('indi_predict_result', 'wb') as f:
@@ -149,7 +203,7 @@ def mf_algo_individual(request):
     # with open('indi_predict_result', 'rb') as f:
     #     data = pickle.load(f)
 
-    return indi_predict_result
+    return predict_result
 
 # 상황 추천 알고리즘
 def mf_condition_recomm_prac(programId):
--- backend/djangoBack/datas/views.py
+++ backend/djangoBack/datas/views.py
@@ -7,14 +7,14 @@ from rest_framework.decorators import api_view
 from datas.models import TbGenre, TbGenreInfo, TbOtt, TbOttInfo, TbProgram, TbRating, TbUser
 from datas.serializers import ProgramSerializer
 from datas import recomm
-from my_settings import API_KEY
+# from my_settings import API_KEY
 
 import requests
 import random
-
+API_KEY = '3beacdbb8f7b35eb8c782851ddc5b403'
 @api_view(['GET'])
 def genre_data(request):
-    res = requests.get('https://api.themoviedb.org/3/genre/tv/list?api_key={API_KEY}&language=ko-kr')
+    res = requests.get('https://api.themoviedb.org/3/genre/tv/list?api_key=3beacdbb8f7b35eb8c782851ddc5b403&language=ko-kr')
     data = res.json()['genres']
     for genre_data in data:
         genre_id = genre_data.get('id')
@@ -26,8 +26,8 @@ def genre_data(request):
         )
     return Response()
 
-def all_program_data():
-    BASE_URL = 'https://api.themoviedb.org/3/tv/popular?api_key={API_KEY}&language=ko-kr&page='
+def all_program_data(request):
+    BASE_URL = 'https://api.themoviedb.org/3/tv/popular?api_key=3beacdbb8f7b35eb8c782851ddc5b403&language=ko-kr&page='
     i = 0
     while True:
         i += 1
@@ -39,65 +39,77 @@ def all_program_data():
             backdrop_path = ''
             program_id=program_data['id']
             if TbProgram.objects.filter(program_num=program_id):
-                continue
-            program_country = program_data['original_language']
-            if program_country == 'ko':
-                program_detail = f'https://api.themoviedb.org/3/tv/{program_id}?api_key={API_KEY}&language=ko-kr'
-                program_ott = f'https://api.themoviedb.org/3/tv/{program_id}/watch/providers?api_key={API_KEY}'
-                detail_res = requests.get(program_detail)
-                ott_res = requests.get(program_ott)
-                data = detail_res.json()
-                ott_data = ott_res.json()['results']
-                original_language = data.get('original_language')   
-                if original_language == 'ko':
-                    title = data.get('name')
-                    if data.get('overview'):
-                        summary = data.get('overview')
-                    networks = data.get('networks')
-                    if data.get('poster_path'):
-                        poster_img = data.get('poster_path')
-                    if data.get('backdrop_path'):
-                        backdrop_path = data.get('backdrop_path')
-                    first_air_date = data.get('first_air_date')
-                    for network in networks:
-                        broadcasting_station = network.get('name')
-                        break
-                    average_rating = data.get('vote_average')
-                    program = TbProgram.objects.create(
-                        program_num = program_id,
-                        title = title,
-                        summary = summary,
-                        broadcasting_station = broadcasting_station,
-                        average_rating = average_rating,
-                        poster_img = 'https://image.tmdb.org/t/p/original' + poster_img,
-                        backdrop_path = 'https://image.tmdb.org/t/p/original' + backdrop_path,
-                        first_air_date = first_air_date
-                    )
-                    for program_genre in data.get('genres'):
-                        if program_genre == None:
-                            break
-                        genre = TbGenreInfo.objects.get(pk=program_genre.get('id'))
-                        TbGenre.objects.create(
-                            program = program,
-                            genre_info = genre
+                program_country = program_data['original_language']
+                if program_country == 'ko':
+                    program_detail = f'https://api.themoviedb.org/3/tv/{program_id}?api_key=3beacdbb8f7b35eb8c782851ddc5b403&language=ko-kr'
+                    detail_res = requests.get(program_detail)
+                    data = detail_res.json()
+                    original_language = data.get('original_language')   
+                    if original_language == 'ko':
+                        if data.get('poster_path'):
+                            poster_img = data.get('poster_path')
+                        program = TbProgram.objects.update(
+                            poster_img = 'https://image.tmdb.org/t/p/w500' + poster_img,
                         )
-                    kr_ott = ott_data.get('KR')
-                    if kr_ott != None:
-                        ott_list = kr_ott.get('flatrate')
-                        if ott_list != None:
-                            for ott_detail in ott_list:
-                                if ott_detail == None:
-                                    break
-                                ott = TbOttInfo.objects.get(pk=ott_detail.get('provider_id'))
-                                TbOtt.objects.create(
-                                    ott_info = ott,
-                                    program = program
-                                )
+            else:
+                program_country = program_data['original_language']
+                if program_country == 'ko':
+                    program_detail = f'https://api.themoviedb.org/3/tv/{program_id}?api_key=3beacdbb8f7b35eb8c782851ddc5b403&language=ko-kr'
+                    program_ott = f'https://api.themoviedb.org/3/tv/{program_id}/watch/providers?api_key=3beacdbb8f7b35eb8c782851ddc5b403'
+                    detail_res = requests.get(program_detail)
+                    ott_res = requests.get(program_ott)
+                    data = detail_res.json()
+                    ott_data = ott_res.json()['results']
+                    original_language = data.get('original_language')   
+                    if original_language == 'ko':
+                        title = data.get('name')
+                        if data.get('overview'):
+                            summary = data.get('overview')
+                        networks = data.get('networks')
+                        if data.get('poster_path'):
+                            poster_img = data.get('poster_path')
+                        if data.get('backdrop_path'):
+                            backdrop_path = data.get('backdrop_path')
+                        first_air_date = data.get('first_air_date')
+                        for network in networks:
+                            broadcasting_station = network.get('name')
+                            break
+                        average_rating = data.get('vote_average')
+                        program = TbProgram.objects.create(
+                            program_num = program_id,
+                            title = title,
+                            summary = summary,
+                            broadcasting_station = broadcasting_station,
+                            average_rating = average_rating,
+                            poster_img = 'https://image.tmdb.org/t/p/w500' + poster_img,
+                            backdrop_path = 'https://image.tmdb.org/t/p/original' + backdrop_path,
+                            first_air_date = first_air_date
+                        )
+                        for program_genre in data.get('genres'):
+                            if program_genre == None:
+                                break
+                            genre = TbGenreInfo.objects.get(pk=program_genre.get('id'))
+                            TbGenre.objects.create(
+                                program = program,
+                                genre_info = genre
+                            )
+                        kr_ott = ott_data.get('KR')
+                        if kr_ott != None:
+                            ott_list = kr_ott.get('flatrate')
+                            if ott_list != None:
+                                for ott_detail in ott_list:
+                                    if ott_detail == None:
+                                        break
+                                    ott = TbOttInfo.objects.get(pk=ott_detail.get('provider_id'))
+                                    TbOtt.objects.create(
+                                        ott_info = ott,
+                                        program = program
+                                    )
     return Response()
 
 @api_view(['GET'])
 def ott_data(request):
-    BASE_URL = "https://api.themoviedb.org/3/watch/providers/tv?api_key={API_KEY}&language=ko-kr&watch_region=KR"
+    BASE_URL = "https://api.themoviedb.org/3/watch/providers/tv?api_key=3beacdbb8f7b35eb8c782851ddc5b403&language=ko-kr&watch_region=KR"
     res = requests.get(BASE_URL)
     ott_list = res.json()['results']
     for ott_data in ott_list:
