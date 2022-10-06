6209eec08edbcaf1eebf2c76a17de8e1876c6770
eunkyung <bbb381@naver.com>
2022-10-07 02:39:05 +0900

    [FE] FIX: 프록시 경로 수정

--- frontend/src/store/socket.js
+++ frontend/src/store/socket.js
@@ -1,6 +1,8 @@
 import SockJS from "sockjs-client";
 import Stomp from "stompjs";
-let url = "http://localhost:8009/api/socket";
+// let url = "http://localhost:8009/api/socket";
+let url = "https://j7b309.p.ssafy.io/api/socket";
+
 let sockJS = new SockJS(url, null, {
   transports: ["websocket", "xhr-streaming", "xhr-polling"],
 });
